a48eb47630e0ae5ed15721ae371f5fe8aebde0d2
Nurulaisyah1 <93265935+Nurulaisyah1@users.noreply.github.com>
2023-10-06 13:46:22 +0700

    Add files via upload

---
+++ kuis.dart
@@ -0,0 +1,25 @@
+void main (){
+  String a = "halo";
+  String b = "dunia";
+  print(a+" "+b);
+}
+
+String c = "Rumahku  diJAkarta";
+
+hasil = c.upper()
+print(hasil)
+kata = c.split(" ")
+hasil2 = " ".join(w.capitalize() for w in kata)
+print(hasil2)
+
+String d = "ini adalah rosid";
+print (d)
+
+  stdout.write("masukkan nama depan :");
+  String nama_depan =
+  stdin.readLineSycn()!;
+  stdout.write("masukkan nama belakang:");
+  String nama_belakang =
+  stdin.readLineSycn()!;
+  print(nama_depan + " " + nama_belakang);
+
